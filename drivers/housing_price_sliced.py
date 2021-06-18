3df2741e2bc093a8cd7dbfa7eebdc4cfb83acac7
Rolando Garcia <rogarcia@berkeley.edu>
2021-06-18 14:51:10 -0700

    updated relative reference to data

--- drivers/housing_price_sliced.py
+++ drivers/housing_price_sliced.py
@@ -15,10 +15,9 @@ with ln.tracking('housing_price'):
 
     dbf = ln.call(dbfread.DBF("../property_example/L3_SHP_M114_Greenfield/M114Assess_CY20_FY20.dbf"),
                   text='dbfread.DBF("./L3_SHP_M114_Greenfield/M114Assess_CY20_FY20.dbf")', line_no=8,
-                  args=[('./L3_SHP_M114_Greenfield/M114Assess_CY20_FY20.dbf', "'./L3_SHP_M114_Greenfield/M114Assess_CY20_FY20.dbf'"),]
                   ).assign(target='dbf') ################################################################################################################
-    dbf = ln.call(iter(dbf), text='iter(dbf)', line_no=9, args=[(dbf, 'dbf')]).assign(target='dbf') #####################################################
-    assets = ln.call(pd.DataFrame(dbf), text='pd.DataFrame(dbf)', line_no=10, args=[(dbf, 'dbf')]).assign(target='assets') ##############################
+    dbf = ln.call(iter(dbf), text='iter(dbf)', line_no=9).assign(target='dbf') #####################################################
+    assets = ln.call(pd.DataFrame(dbf), text='pd.DataFrame(dbf)', line_no=10).assign(target='assets') ##############################
 
 
     """
@@ -27,47 +26,31 @@ with ln.tracking('housing_price'):
     """
 
     def get_threshold(): ###############################################################################################
-        with ln.func(name='get_threshold', args=None, ret_text='1970', line_no=12):
+        with ln.func(name='get_threshold', ret_text='1970', line_no=12):
             return 1970
     ln.assign(get_threshold, 'get_threshold', 'get_threshold', line_no=21)
 
     def is_new(col): ###################################################################################################
-        with ln.func(name='is_new', args=[(col, 'col'),], ret_text='col > get_threshold()', line_no=13):
+        with ln.func(name='is_new', ret_text='col > get_threshold()', line_no=13):
             return col > ln.call(get_threshold(), text='get_threshold()', line_no=14)
     ln.assign(is_new, 'is_new', 'is_new', line_no=22)
 
     assets['is_new'] = ln.call(is_new(assets['YEAR_BUILT']),
-                               text="is_new(assets['YEAR_BUILT'])", line_no=15, args=[(assets['YEAR_BUILT'],
-                                                                                       "assets['YEAR_BUILT']")]
+                               text="is_new(assets['YEAR_BUILT'])", line_no=15
                                ).assign(target="assets") ###############################################################
 
     clf = ln.call(RandomForestClassifier(random_state=0),
                   text='RandomForestClassifier(random_state=0)',
-                  line_no=16,
-                  args=[(0, {'random_state': '0'}), ]
+                  line_no=16
                   ).assign(target='clf') ###############################################################################
     # assign(e, target, text, line_no, mod=None):
     y = ln.assign(assets['is_new'], text="assets", target='y', line_no=17) #############################################
     x = ln.assign(assets[['BLDG_VAL', 'LOT_SIZE', 'NUM_ROOMS']],
                   text="assets", line_no=18, target='x')                   #############################################
 
-    ln.call(clf.fit(x, y), text="clf.fit(x, y)", line_no=19, args=[(x, 'x'), (y, 'y')]) ################################
+    ln.call(clf.fit(x, y), text="clf.fit(x, y)", line_no=19) ################################
 
     p = ln.call(clf.predict([[100 * 1000, 10, 4]]),
                 text="clf.predict([[100 * 1000, 10, 4]])",
-                line_no=20,
-                args=[([[100 * 1000, 10, 4]], "[[100 * 1000, 10, 4]]")]
+                line_no=20
                 ).assign(target='p') ###################################################################################
-
-    # import sqlite3
-    # ln.importing(sqlite3, module='sqlite3', name='sqlite3', line_no=21)
-    #
-    #
-    # con = ln.call(sqlite3.connect('demo.db'), text="sqlite3.connect('demo.db')", line_no=22, args=[('demo.db',
-    #                                                                                                 "'demo.db'")]
-    #               ).assign(target='con')
-    # ln.call(pd.read_sql('SELECT * FROM data_assets', con),
-    #         text="pd.read_sql('SELECT * FROM data_assets', con)",
-    #         line_no=23,
-    #         args=[('SELECT * FROM data_assets', "'SELECT * FROM data_assets'"),
-    #               (con, 'con')])
--- drivers/housing_price_transformed.py
+++ drivers/housing_price_transformed.py
@@ -1,57 +1,74 @@
 import gadget as ln
 with ln.tracking('housing_price'):
     import altair as alt
-    ln.importing(alt, module='altair', name='alt', line_no=1)
     import dbfread
-    ln.importing(dbfread, module='dbfread', name='dbfread', line_no=2)
     import pandas as pd
-    ln.importing(pd, module='pandas', name='pd', line_no=3)
     import seaborn as sns
-    ln.importing(sns, module='seaborn', name='sns', line_no=4)
     from sklearn.ensemble import RandomForestClassifier
-    ln.importing(RandomForestClassifier, module='sklearn.ensemble', name=
-        'RandomForestClassifier', line_no=5)
-    ln.call(alt.data_transformers.enable('json'), text=
-        "alt.data_transformers.enable('json')", line_no=7)
-    ln.call(alt.renderers.enable('mimetype'), text=
-        "alt.renderers.enable('mimetype')", line_no=8)
-    dbf = ln.call(dbfread.DBF(
-        '../property_example/L3_SHP_M114_Greenfield/M114Assess_CY20_FY20.dbf'
-        ), text=
-        """dbfread.DBF(
-    '../property_example/L3_SHP_M114_Greenfield/M114Assess_CY20_FY20.dbf')"""
-        , line_no=10).assign(target='dbf')
-    dbf = ln.call(iter(dbf), text='iter(dbf)', line_no=12).assign(target='dbf')
-    assets = ln.call(pd.DataFrame(dbf), text='pd.DataFrame(dbf)', line_no=13
-        ).assign(target='assets')
-    ln.call(sns.relplot(data=assets, x='YEAR_BUILT', y='BLDG_VAL', size=
-        'LOT_SIZE'), text=
-        "sns.relplot(data=assets, x='YEAR_BUILT', y='BLDG_VAL', size='LOT_SIZE')"
-        , line_no=15)
+
+    ln.importing(alt, module='altair', name='alt', line_no=1)
+    ln.importing(dbfread, module='dbfread', name='dbfread', line_no=2)
+    ln.importing(pd, module='pandas', name='pd', line_no=3)
+    ln.importing(sns, module='seaborn', name='sns', line_no=4)
+    ln.importing(RandomForestClassifier, module='sklearn.ensemble.RandomForestClassifier', name='RandomForestClassifier', line_no=5)
+
+    # Set up some config
+    # ln.call(pe=, text=, line_no=, args=)
+    ln.call(alt.data_transformers.enable('json'), text="alt.data_transformers.enable('json')", line_no=6)
+    ln.call(alt.renderers.enable('mimetype'), text="alt.renderers.enable('mimetype')", line_no=7)
+
+    dbf = ln.call(dbfread.DBF("../../tracer/property_example/L3_SHP_M114_Greenfield/M114Assess_CY20_FY20.dbf"),
+                  text='dbfread.DBF("./L3_SHP_M114_Greenfield/M114Assess_CY20_FY20.dbf")', line_no=8
+                  ).assign(target='dbf')
+    dbf = ln.call(iter(dbf), text='iter(dbf)', line_no=9).assign(target='dbf')
+    assets = ln.call(pd.DataFrame(dbf), text='pd.DataFrame(dbf)', line_no=10).assign(target='assets')
+
+    ln.call(
+        sns.relplot(
+            data=assets,
+            x="YEAR_BUILT", y="BLDG_VAL", size='LOT_SIZE'
+        ),
+        text="""
+            sns.relplot(
+            data=assets,
+            x="YEAR_BUILT", y="BLDG_VAL", size='LOT_SIZE'
+        )
+        """,
+        line_no=11
+    )
+
+    """
+    Just make sure that it is not ambiguous which of the possibly many functions is running
+    assuming there could be name colisions.
+    """
 
     def get_threshold():
-        with ln.func(name='get_threshold', args=[], ret_text='(1970)',
-            line_no=18):
+        with ln.func(name='get_threshold', ret_text='1970', line_no=12):
             return 1970
-    ln.assign(get_threshold, text='get_threshold', line_no=18, target=
-        'get_threshold')
+    ln.assign(get_threshold, 'get_threshold', 'get_threshold', line_no=21)
 
     def is_new(col):
-        with ln.func(name='is_new', args=[(col, 'col')], ret_text=
-            '(col > get_threshold())', line_no=21):
-            return col > get_threshold()
-    ln.assign(is_new, text='is_new', line_no=21, target='is_new')
-    assets['is_new'] = ln.call(is_new(assets['YEAR_BUILT']), text=
-        "is_new(assets['YEAR_BUILT'])", line_no=24).assign(target=
-        "assets['is_new']")
-    clf = ln.call(RandomForestClassifier(random_state=0), text=
-        'RandomForestClassifier(random_state=0)', line_no=26).assign(target
-        ='clf')
-    y = ln.assign(assets['is_new'], text="assets['is_new']", line_no=27,
-        target='y')
-    x = ln.assign(assets[['BLDG_VAL', 'LOT_SIZE', 'NUM_ROOMS']], text=
-        "assets[['BLDG_VAL', 'LOT_SIZE', 'NUM_ROOMS']]", line_no=28, target='x'
-        )
-    ln.call(clf.fit(x, y), text='clf.fit(x, y)', line_no=30)
-    p = ln.call(clf.predict([[100 * 1000, 10, 4]]), text=
-        'clf.predict([[100 * 1000, 10, 4]])', line_no=31).assign(target='p')
+        with ln.func(name='is_new', ret_text='col > get_threshold()', line_no=13):
+            return col > ln.call(get_threshold(), text='get_threshold()', line_no=14)
+    ln.assign(is_new, 'is_new', 'is_new', line_no=22)
+
+    assets['is_new'] = ln.call(is_new(assets['YEAR_BUILT']),
+                               text="is_new(assets['YEAR_BUILT'])", line_no=15).assign(target="assets['is_new']")
+
+    clf = ln.call(RandomForestClassifier(random_state=0),
+                  text='RandomForestClassifier(random_state=0)',
+                  line_no=16
+                  ).assign(target='clf')
+    # assign(e, target, text, line_no, mod=None):
+    y = ln.assign(assets['is_new'], text="assets['is_new']", target='y', line_no=17)
+    x = ln.assign(assets[['BLDG_VAL', 'LOT_SIZE', 'NUM_ROOMS']],
+                  text="assets[['BLDG_VAL', 'LOT_SIZE', 'NUM_ROOMS']]", line_no=18, target='x')
+
+    ln.call(clf.fit(x, y), text="clf.fit(x, y)", line_no=19)
+
+    p = ln.call(clf.predict([[100 * 1000, 10, 4]]),
+                text="clf.predict([[100 * 1000, 10, 4]])",
+                line_no=20,
+                ).assign(target='p')
+
+
